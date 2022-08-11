fe512fb73e432b3085c41d6c65c8ee92dad76fdc
Esteban Beltran <estebanenriquebeltran@gmail.com>
2022-08-10 23:45:04 -0400

    Add factories data with faker

--- src/test/NewMedicalRecord.test.tsx
+++ src/test/NewMedicalRecord.test.tsx
@@ -7,67 +7,63 @@ import React from 'react'
 import { getMedicalRecord } from '../../mocks/handlers'
 import { server } from '../../mocks/server'
 import NewMedicalRecord from '../components/newMedicalRecord'
-import type { Medicine } from '../models'
 
+import { generateMedicine, generateMedicineList } from './factories/medicine'
 import { renderWrapper } from './utils'
 
-describe('Medical Record Form', () => {
-  test('Type observations', async () => {
-    const user = userEvent.setup()
+test('Show select with all medicines', async () => {
+  const user = userEvent.setup()
+  const medicines = generateMedicineList()
 
-    const ui = renderWrapper()
-    act(() => {
-      NiceModal.show(NewMedicalRecord)
-    })
-    await user.type(ui.getByLabelText('Observaciones'), 'Hola')
-
-    expect(ui.getByLabelText('Observaciones')).toHaveValue('Hola')
+  server.use(getMedicalRecord({ medicines }))
+  const ui = renderWrapper()
+  act(() => {
+    NiceModal.show(NewMedicalRecord)
   })
 
-  test('Submit form with completed data', async () => {
-    const submitMock = jest.fn()
-    const user = userEvent.setup()
-    const medicine: Medicine = {
-      createdAt: '2022-06-15T01:36:37.805Z',
-      dose: '2',
-      id: 1,
-      management: 'oral',
-      name: 'Paracetamol',
-      updatedAt: '2022-06-15T01:36:37.805Z',
-    }
+  await user.click(await screen.findByLabelText('Medicamentos'))
 
-    server.use(getMedicalRecord({ medicines: [medicine] }))
-    renderWrapper()
+  medicines.forEach((medicine) =>
+    expect(ui.getByText(medicine.name)).toBeInTheDocument()
+  )
+})
 
-    act(() => {
-      NiceModal.show(NewMedicalRecord, {
-        defaultValues: { endDate: new Date(), startDate: new Date() },
-        onSubmit: submitMock,
-      })
+test('Submit form with completed data', async () => {
+  const submitMock = jest.fn()
+  const user = userEvent.setup()
+  const medicine = generateMedicine()
+
+  server.use(getMedicalRecord({ medicines: [medicine] }))
+  renderWrapper()
+
+  act(() => {
+    NiceModal.show(NewMedicalRecord, {
+      defaultValues: { endDate: new Date(), startDate: new Date() },
+      onSubmit: submitMock,
     })
+  })
 
-    const startDate = '08-07-2022'
-    const endDate = '08-14-2022'
+  const startDate = '08-07-2022'
+  const endDate = '08-14-2022'
 
-    await user.click(await screen.findByLabelText('Medicamentos'))
-    await user.click(screen.getByText('Paracetamol'))
+  await user.click(await screen.findByLabelText('Medicamentos'))
+  await user.click(screen.getByText(medicine.name))
 
-    await user.click(screen.getByLabelText('Desde'))
-    await user.keyboard('{Backspace>8}')
-    await user.type(screen.getByLabelText('Desde'), startDate)
+  await user.click(screen.getByLabelText('Desde'))
+  await user.keyboard('{Backspace>8}')
+  await user.type(screen.getByLabelText('Desde'), startDate)
 
-    await user.click(screen.getByLabelText('Hasta'))
-    await user.keyboard('{Backspace>8}')
-    await user.type(screen.getByLabelText('Hasta'), endDate)
+  await user.click(screen.getByLabelText('Hasta'))
+  await user.keyboard('{Backspace>8}')
+  await user.type(screen.getByLabelText('Hasta'), endDate)
 
-    await user.click(screen.getByText('Submit'))
+  await user.click(screen.getByText('Submit'))
 
-    expect(submitMock).toHaveBeenCalledTimes(1)
-    expect(submitMock).toHaveBeenCalledWith({
-      endDate: new Date(endDate),
-      medicinesSelected: [{ id: medicine.id, label: medicine.name }],
-      observations: '',
-      startDate: new Date(startDate),
-    })
+  expect(submitMock).toHaveBeenCalledTimes(1)
+  expect(submitMock).toHaveBeenCalledWith({
+    endDate: new Date(endDate),
+    medicinesSelected: [{ id: medicine.id, label: medicine.name }],
+    observations: '',
+    startDate: new Date(startDate),
   })
 })
---
+++ src/test/factories/medicine.ts
@@ -0,0 +1,26 @@
+import { faker } from '@faker-js/faker'
+import type { BuildOptions } from 'fishery'
+import { Factory } from 'fishery'
+
+import type { Medicine } from '../../models'
+
+const medicineFactory = Factory.define<Medicine>(({ sequence }) => ({
+  createdAt: faker.date.past().toISOString(),
+  dose: faker.random.word(),
+  id: sequence,
+  management: faker.random.word(),
+  name: faker.random.word(),
+  updatedAt: faker.date.past().toISOString(),
+}))
+
+export const generateMedicine = (params?: Partial<Medicine>) => {
+  return medicineFactory.build(params)
+}
+
+export const generateMedicineList = (
+  number = 10,
+  params?: Partial<Medicine>,
+  options?: BuildOptions<Medicine, any>
+) => {
+  return medicineFactory.buildList(number, params, options)
+}
